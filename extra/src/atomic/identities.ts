faaadf5af739b39a30291efcb24233d28e24ab8e
kiki <lin912305043@gmail.com>
2023-05-08 17:18:36 +0800

    typo

--- extra/src/atomic/identities.ts
+++ extra/src/atomic/identities.ts
@@ -2,8 +2,9 @@
 export enum LanguageIdentities {
   // 英语 (原始语言)
   "us" = "us",
+  // 简体和繁体中文标识来源参见 Language Tags 部分 https://sites.psu.edu/symbolcodes/languages/asia/chinese/
   // 简体中文
-  "zh-hans" = "zh-hans",
+  "zh-hans" = "zh-Hans",
   // 繁体中文
-  "zh-hant" = "zh-hant",
+  "zh-hant" = "zh-Hant",
 }
